94fb9bdec717754ae89d33cddd4112711746d491
Mauricio Javier Montenegro <mjmontenegro@gmail.com>
2019-03-19 05:27:55 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,22 @@ const codes = [
 
 function init() {
   // your code here
+  document.querySelector('body').addEventListener('keydown', onKeyDownHandler);
 }
+
+let index = 0
+
+function onKeyDownHandler(e) {
+  const key = e.key;
+
+  if( key === codes[index]) {
+    index++;
+    if( index === codes.length) {
+      alert("Three Extra Lives!!!");
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
+init();
